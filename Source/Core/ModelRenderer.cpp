243aad66c87e1f7838609167e685bc4bf45c2ffb
Samuel Rasquinha <samuelrasquinha@gmail.com>
2021-08-11 21:29:34 +0530

    Update ModelRenderer.cpp

--- Source/Core/ModelRenderer.cpp
+++ Source/Core/ModelRenderer.cpp
@@ -9,31 +9,35 @@ void Lumen::RenderEntity(Entity& entity, GLClasses::Shader& shader)
 	shader.SetMatrix4("u_ModelMatrix", entity.m_Model);
 	shader.SetMatrix3("u_NormalMatrix", glm::mat3(glm::transpose(glm::inverse(entity.m_Model))));
 
+	int DrawCalls = 0;
+
 	for (auto& e : object->m_Meshes)
 	{
+
+
 		const Mesh* mesh = &e;
 
-		if (mesh->m_AlbedoMap.GetID() > 0)
+		if (mesh->m_AlbedoMap.GetID() != 0)
 		{
 			mesh->m_AlbedoMap.Bind(0);
 		}
 
-		if (mesh->m_NormalMap.GetID() > 0)
+		if (mesh->m_NormalMap.GetID() != 0)
 		{
 			mesh->m_NormalMap.Bind(1);
 		}
 
-		if (mesh->m_MetalnessMap.GetID() > 0)
+		if (mesh->m_MetalnessMap.GetID() != 0)
 		{
 			mesh->m_MetalnessMap.Bind(3);
 		}
 
-		if (mesh->m_RoughnessMap.GetID() > 0)
+		if (mesh->m_RoughnessMap.GetID() != 0)
 		{
 			mesh->m_RoughnessMap.Bind(4);
 		}
 
-		if (mesh->m_AmbientOcclusionMap.GetID() > 0)
+		if (mesh->m_AmbientOcclusionMap.GetID() != 0)
 		{
 			mesh->m_AmbientOcclusionMap.Bind(5);
 		}
@@ -43,15 +47,22 @@ void Lumen::RenderEntity(Entity& entity, GLClasses::Shader& shader)
 
 		if (mesh->m_Indexed)
 		{
+			DrawCalls++;
 			glDrawElements(GL_TRIANGLES, mesh->m_IndicesCount, GL_UNSIGNED_INT, 0);
 		}
 
 		else
 		{
+			DrawCalls++;
 			glDrawArrays(GL_TRIANGLES, 0, mesh->m_VertexCount);
 		}
 
 		VAO.Unbind();
 	}
 
+
+	if (std::fmod(glfwGetTime(), 0.5f) < 0.001f)
+	{
+		std::cout << "\nDRAW CALLS : " << DrawCalls;
+	}
 }
